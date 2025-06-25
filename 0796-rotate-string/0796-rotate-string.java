3f613e8f6ff325e09798c01c4793f6081ff311c7
Shreyanshi Singhal <144926708+Shreya2026@users.noreply.github.com>
2025-06-25 23:28:12 +0530

    Time: 0 ms (100%), Space: 41.7 MB (39.98%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,18 @@
+/*class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        for(int i=0;i<s.length();i++){
+            String str=s.substring(i)+s.substring(0,i);
+            if(str.equals(goal)) return true;
+        }
+        return false;
+    }
+}*/
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        String combined=s+s;
+        return combined.contains(goal);
+    }
+}
