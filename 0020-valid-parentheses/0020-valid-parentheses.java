8cce9f7cfce42fcbadc105bb2cc485ce553e008b
Shreyanshi Singhal <144926708+Shreya2026@users.noreply.github.com>
2025-07-25 19:50:35 +0530

    Time: 2 ms (96.65%), Space: 42 MB (31.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -17,3 +17,29 @@ class Solution {
         return st.isEmpty();
     }
 }
+
+//minor cleanup for interviews
+/*
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) return false;
+
+                char top = st.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
+*/
