9ff11aeff1e74f8e8640976585854a048d2ca135
Shreyanshi Singhal <144926708+Shreya2026@users.noreply.github.com>
2025-08-27 23:45:09 +0530

    Time: 1 ms (87.06%), Space: 47.8 MB (55.06%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,58 @@
+//DFS SOLUTION
+//Time Complexity: O(n²) (since we may traverse the whole matrix).
+//Space Complexity: O(n) (visited array + recursion stack).
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(isConnected, visited, i);
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+
+    private void dfs(int[][] isConnected,boolean[] visited,int city){
+        visited[city]=true;
+        for(int j=0;j<isConnected.length;j++){
+            if (isConnected[city][j] == 1 && !visited[j]) {
+                dfs(isConnected, visited, j);
+        }
+    }
+    }
+}
+
+
+
+/*
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                Queue<Integer> queue = new LinkedList<>();
+                queue.offer(i);
+                while (!queue.isEmpty()) {
+                    int city = queue.poll();
+                    visited[city] = true;
+                    for (int j = 0; j < n; j++) {
+                        if (isConnected[city][j] == 1 && !visited[j]) {
+                            queue.offer(j);
+                        }
+                    }
+                }
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+}*/
+
