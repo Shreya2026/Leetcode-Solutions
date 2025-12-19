fccab423b264c50eb0902c7900efb1428e196b1b
Shreyanshi Singhal <144926708+Shreya2026@users.noreply.github.com>
2025-12-19 19:12:22 +0530

    Time: 8 ms (44.39%), Space: 46.3 MB (21.39%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        int n = s.length();
+
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true; // empty string
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
+
+/*
+| Type  | Complexity                        |
+| ----- | --------------------------------- |
+| Time  | **O(n²)** (substring + dp checks) |
+| Space | **O(n)**                          |
+
+*/
