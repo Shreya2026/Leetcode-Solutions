75a5c652bd4b9a5d29421bab23be7129efcae8cc
Shreyanshi Singhal <144926708+Shreya2026@users.noreply.github.com>
2025-12-19 19:36:14 +0530

    Time: 19 ms (11.08%), Space: 50.1 MB (56.85%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.Stack;
+
+class BSTIterator {
+
+    private Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        pushLeft(root);
+    }
+
+    private void pushLeft(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+
+    public int next() {
+        TreeNode curr = stack.pop();
+        if (curr.right != null) {
+            pushLeft(curr.right);
+        }
+        return curr.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
+
+
+/*
+Time Complexity
+
+next() → O(1) amortized
+
+hasNext() → O(1)
+
+Space Complexity
+
+O(H)
+
+H = height of tree (worst case O(N), balanced O(log N))
+*/
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
